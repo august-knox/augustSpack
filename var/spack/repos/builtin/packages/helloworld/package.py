8bf7750f4eef7affdf7fe2f4ce9ad1522c7a717c
August George Knox <knox10@ruby964.llnl.gov>
2024-06-14 10:48:37 -0700

    fixed compiler?

--- var/spack/repos/builtin/packages/helloworld/package.py
+++ var/spack/repos/builtin/packages/helloworld/package.py
@@ -38,7 +38,7 @@ class Helloworld(MakefilePackage):
     # See https://spdx.org/licenses/ for a list. Upon manually verifying
     # the license, set checked_by to your Github username.
     license("UNKNOWN", checked_by="github_user1")
-
+    version("1.5", sha256="c0a3458fd151af7471cc31d250a79b1850567b9c901741e96ad8a5e1c6df2a68")
     version("1.4", sha256="7ef028b04ce70fbeb997cb3d5fc14d93d288d1964bb2e848f082debf93db6fef")
     version("1.3", sha256="7e9034d08a6132309e23085e4e14218e9e088594adb53cdf9a56b2bed8d365f6")
     version("1.2", sha256="18cfa58756cf99d4322147b708bef2ba80ae90b5356e0ffbe0e2438efbc113e8")
@@ -49,4 +49,4 @@ class Helloworld(MakefilePackage):
 
     def install(self, spec, prefix):
         mkdir(prefix.bin)
-        install("hw", prefix.bin)
+        install("helloworld", prefix.bin)
