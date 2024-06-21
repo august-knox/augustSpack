1ffdf78eeb3907d8cc523720bec85718f0e20b24
August George Knox <knox10@poodle17.llnl.gov>
2024-06-21 10:22:36 -0700

    new helloworld package

--- var/spack/repos/builtin/packages/helloworld/package.py
+++ var/spack/repos/builtin/packages/helloworld/package.py
@@ -28,7 +28,7 @@ class Helloworld(MakefilePackage):
 
     # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://www.example.com"
-    url = "https://github.com/august-knox/helloWorld/archive/refs/tags/v1.4.tar.gz"
+    url = "https://github.com/august-knox/helloWorld/archive/refs/tags/v1.6.tar.gz"
 
     # FIXME: Add a list of GitHub accounts to
     # notify when the package is updated.
@@ -38,6 +38,7 @@ class Helloworld(MakefilePackage):
     # See https://spdx.org/licenses/ for a list. Upon manually verifying
     # the license, set checked_by to your Github username.
     license("UNKNOWN", checked_by="github_user1")
+    version("1.6", sha256="e11e61f993cc4f1abd6618ebbf94b0cde8f370b41b3a0703b901517be940996f")
     version("1.5", sha256="c0a3458fd151af7471cc31d250a79b1850567b9c901741e96ad8a5e1c6df2a68")
     version("1.4", sha256="7ef028b04ce70fbeb997cb3d5fc14d93d288d1964bb2e848f082debf93db6fef")
     version("1.3", sha256="7e9034d08a6132309e23085e4e14218e9e088594adb53cdf9a56b2bed8d365f6")
